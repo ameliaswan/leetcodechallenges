2976687f6d64902fe848be08128ca88cae7e693d
ameliaswan <kooniwang@foxmail.com>
2023-02-19 23:56:04 -0800

    longestCommonPrefix_binarysearch

--- src/dataStructures/string/LongestCommonPrefix_14.java
+++ src/dataStructures/string/LongestCommonPrefix_14.java
@@ -1,11 +1,17 @@
 package dataStructures.string;
 
 public class LongestCommonPrefix_14 {
+    /**
+     * This method uses the substring of 1st element to check if it's common prefix of rest of elements in input param.
+     * @param strs
+     * the input array of String
+     * @return the longestCommonPrefix
+     */
     public String longestCommonPrefix(String[] strs) {
         if(strs.length == 0){
             return "";
         }
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         int len = strs[0].length();
         for(int i = 0; i < len ; i++){
             char ch = strs[0].charAt(i);
@@ -16,9 +22,64 @@ public class LongestCommonPrefix_14 {
                 }
                 j++;
             }
-            sb.append(String.valueOf(ch));
+            sb.append(ch);
         }
 
         return sb.toString();
+    }
+
+    /**
+     * This method uses binary search to find the longest common prefix.
+     * Based on the shortest String (since the minlen is the upper limit length of common prefix).
+     * @param strs
+     * the input array of String
+     * @return the longestCommonPrefix
+     * begin: the beginning index of common prefix
+     * end: the end index of common prefix
+     * The convertable str will be strs[0].substring(0, mid+1)
+     * loop each elements in strs to check if current substring is common then change begin/end
+     * Time complexity is O(n * log(minlen))
+     * Space complexity is O(1)
+     */
+
+    public String longestCommonPrefix_binarysearch(String[] strs) {
+        if(strs.length == 0){
+            return "";
+        }
+        String sb = strs[0];
+        int minlen = Integer.MAX_VALUE;
+        for(String str : strs){
+            minlen = Math.min(str.length(), minlen);
+        }
+        int begin = 0, end = minlen-1, mid = begin + (end-begin)/2;
+        while(begin <= end){
+            if(isCommonPrefix(mid, strs)){
+                begin = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+            mid = begin + (end-begin)/2;
+        }
+        return sb.substring(0, begin);
+    }
+
+    /**
+     * This method checks if strs[0].substring(0, mid+1) is common prefix for the rest of elements in strs
+     * @param mid
+     * The middle index
+     * @param strs
+     * the input array of String
+     * @return true/false
+     */
+    private boolean isCommonPrefix(int mid, String[] strs){
+        String commonstr = strs[0].substring(0, mid+1);
+        int j = 1;
+        while(j < strs.length){
+            if(strs[j].indexOf(commonstr) != 0){
+                return false;
+            }
+            j++;
+        }
+        return true;
     }
 }
--- test/dataStructures/string/Test_LongestCommonPrefix_14.java
+++ test/dataStructures/string/Test_LongestCommonPrefix_14.java
@@ -7,21 +7,31 @@ public class Test_LongestCommonPrefix_14 {
     @Test
     public void testCase1(){
         String[] strs = {"flower","flow","flight"};
+        //method1
         LongestCommonPrefix_14 var = new LongestCommonPrefix_14();
         String result = var.longestCommonPrefix(strs);
+        //binary search
+        String bsResult = var.longestCommonPrefix_binarysearch(strs);
+
         String expected = "fl";
 
         Assertions.assertEquals(result, expected);
+        Assertions.assertEquals(bsResult, expected);
     }
 
     @Test
     public void testCase2(){
         String[] strs = {"dog","racecar","car"};
+        //method1
         LongestCommonPrefix_14 var = new LongestCommonPrefix_14();
         String result = var.longestCommonPrefix(strs);
+        //binary search
+        String bsResult = var.longestCommonPrefix_binarysearch(strs);
+
         String expected = "";
 
         Assertions.assertEquals(result, expected);
+        Assertions.assertEquals(result, bsResult);
     }
 
 }
